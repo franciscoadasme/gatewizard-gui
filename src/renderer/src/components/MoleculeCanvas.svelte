a4770767987b3e3cb053470ecfe3f055fc30e5ca
Francisco Adasme <francisco.adasme@gmail.com>
2026-04-09 18:07:10 -0400

    fix: Remove damping from orbit controls

--- src/renderer/src/components/MoleculeCanvas.svelte
+++ src/renderer/src/components/MoleculeCanvas.svelte
@@ -20,7 +20,7 @@
     <T.Color attach="background" args={[0x000000]} />
     <T.PerspectiveCamera makeDefault position={[2.8, 2.2, 2.8]} fov={45} />
     <MoleculeCameraRig {structure} />
-    <OrbitControls enableDamping dampingFactor={0.08} />
+    <OrbitControls enableDamping={false} />
     <T.AmbientLight intensity={0.45} />
     <T.DirectionalLight position={[5, 8, 4]} intensity={1.1} />
     {#if hasStructure}
