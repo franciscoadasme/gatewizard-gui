ef1dbb75f7d3b4ef0aceb5438f1a95d569c9a24f
Francisco Adasme <francisco.adasme@gmail.com>
2026-04-23 18:30:56 -0400

    feat: Add Empty component

---
+++ src/renderer/src/components/ui/Empty.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let message = 'No data available'
+  export let className = ''
+
+  const baseClass =
+    'flex flex-1 items-center justify-center rounded-lg border border-dashed border-neutral-800 text-neutral-700'
+
+  $: classes = `${baseClass} ${className}`.trim()
+</script>
+
+<p class={classes} {...$$restProps}>
+  {message}
+</p>
