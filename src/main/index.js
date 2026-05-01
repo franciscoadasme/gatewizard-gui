1bdc321c091417a21e08360dbc302719e87efde0
Mauricio Bedoya <62667433+maurobedoya@users.noreply.github.com>
2026-04-30 22:35:39 -0400

    feat: add multi-file open dialog

--- src/main/index.js
+++ src/main/index.js
@@ -271,6 +271,25 @@ ipcMain.handle('dialog:openFile', async (_event, title, filters, defaultPath = u
   return { canceled: false, filePath: result.filePaths[0] }
 })
 
+ipcMain.handle('dialog:openFiles', async (_event, title, filters, defaultPath = undefined) => {
+  filters = filters || []
+  if (!filters.some((filter) => filter.name.toLowerCase() === 'all files')) {
+    filters.push({ name: 'All files', extensions: ['*'] })
+  }
+
+  const win = BrowserWindow.getFocusedWindow()
+  const result = await dialog.showOpenDialog(win ?? undefined, {
+    title: title || 'Open Files',
+    filters,
+    defaultPath,
+    properties: ['openFile', 'multiSelections']
+  })
+  if (result.canceled || result.filePaths.length === 0) {
+    return { canceled: true, filePaths: [] }
+  }
+  return { canceled: false, filePaths: result.filePaths }
+})
+
 ipcMain.handle('fs:readJson', async (_event, filePath) => {
   const contents = await readFile(filePath, 'utf-8')
   return JSON.parse(contents)
--- src/preload/index.js
+++ src/preload/index.js
@@ -5,6 +5,8 @@ import { electronAPI } from '@electron-toolkit/preload'
 const api = {
   openFileDialog: (title, filters, defaultPath = undefined) =>
     ipcRenderer.invoke('dialog:openFile', title, filters, defaultPath),
+  openFilesDialog: (title, filters, defaultPath = undefined) =>
+    ipcRenderer.invoke('dialog:openFiles', title, filters, defaultPath),
   openPdbDialog: () => ipcRenderer.invoke('dialog:openPdb'),
   openDirectoryDialog: (title = 'Select Directory', defaultPath = undefined) =>
     ipcRenderer.invoke('dialog:openDirectory', title, defaultPath),
