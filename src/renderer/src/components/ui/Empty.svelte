c55de4f3529b1338b54c7dfdbfe7eec13e4788ac
Francisco Adasme <francisco.adasme@gmail.com>
2026-05-10 10:47:07 -0400

    fix: Add missing padding and centering to Empty frame

--- src/renderer/src/components/ui/Empty.svelte
+++ src/renderer/src/components/ui/Empty.svelte
@@ -3,7 +3,7 @@
   export let className = ''
 
   const baseClass =
-    'flex flex-1 items-center justify-center rounded-lg border border-dashed border-neutral-800 text-neutral-700'
+    'flex flex-1 items-center justify-center rounded-lg border border-dashed border-neutral-800 text-neutral-700 text-center p-2'
 
   $: classes = `${baseClass} ${className}`.trim()
 </script>
