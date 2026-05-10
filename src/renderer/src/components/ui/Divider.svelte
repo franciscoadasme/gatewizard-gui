4e589e7e90da187178bd669b4dc10f4ecfdf1de8
Francisco Adasme <francisco.adasme@gmail.com>
2026-05-10 11:54:17 -0400

    feat: Add optional message to Divider

--- src/renderer/src/components/ui/Divider.svelte
+++ src/renderer/src/components/ui/Divider.svelte
@@ -1,1 +1,13 @@
-<hr class="dark:border-neutral-800" />
+<script>
+  let { className = '', message = '' } = $props()
+</script>
+
+{#if message}
+  <div class="flex items-center gap-2">
+    <hr class="flex-1 dark:border-neutral-800" />
+    <span class="text-xs text-neutral-500">{message}</span>
+    <hr class="flex-1 dark:border-neutral-800" />
+  </div>
+{:else}
+  <hr class="dark:border-neutral-800" />
+{/if}
